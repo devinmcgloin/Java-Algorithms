dee74c1ce036a9b62717836256e4178047c47951
Devin McGloin <devin@devinmcgloin.com>
2015-10-17 15:26:14 -0400

    made binary search more efficient

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -10,51 +10,51 @@ public class BinarySearch {
     public static <T extends Comparable<T>> int recSearch(ArrayList<T> list, T searchTerm) {
         int low = 0;
         int high = list.size() - 1;
-        int middle = 0;
+        int middle;
         if (high < low) {
             return -(low + 1);
         }
-        middle = (low + high) / 2;
 
-        if (list.get(middle).equals(searchTerm)) {
-            return middle;
-        } else if (list.get(middle).compareTo(searchTerm) < 0) {
+        middle = (low + high) >> 1; //bit shift divide by two.
+        T temp = list.get(middle);
+        int comp = temp.compareTo(searchTerm);
+
+        if (comp < 0) {
             return recSearch(list, searchTerm, middle + 1, high);
-        } else if (list.get(middle).compareTo(searchTerm) > 0) {
+        } else if (comp > 0) {
             return recSearch(list, searchTerm, low, middle - 1);
-        }
-        //Shouldn't never reach this
-        return -(low + 1);
+        } else return middle;
     }
 
     private static <T extends Comparable<T>> int recSearch(ArrayList<T> list, T searchTerm, int low, int high) {
-        int middle = (low + high) / 2;
+        int middle = (low + high) >> 1; //bit shift divide by two.
+        T temp = list.get(middle);
+        int comp = temp.compareTo(searchTerm);
+
         if (high < low) {
             return -(low + 1);
-        } else if (list.get(middle).equals(searchTerm)) {
-            return middle;
-        } else if (list.get(middle).compareTo(searchTerm) < 0) {
-            return recSearch(list, searchTerm, middle + 1, high);
-        } else if (list.get(middle).compareTo(searchTerm) > 0) {
-            return recSearch(list, searchTerm, low, middle - 1);
         }
-        //Shouldn't ever reach this
-        return -(low + 1);
+        if (comp < 0) {
+            return recSearch(list, searchTerm, middle + 1, high);
+        } else if (comp > 0) {
+            return recSearch(list, searchTerm, low, middle - 1);
+        } else return middle;
     }
 
     public static <T extends Comparable<T>> int search(ArrayList<T> list, T searchTerm) {
         int low = 0;
         int high = list.size() - 1;
-        int middle = 0;
+        int middle;
+
         while (high >= low) {
             middle = (low + high) >> 1; //bit shift divide by two.
-            if (list.get(middle).equals(searchTerm)) {
-                return middle;
-            } else if (list.get(middle).compareTo(searchTerm) < 0) {
+            T temp = list.get(middle);
+            int comp = temp.compareTo(searchTerm);
+            if (comp < 0) {
                 low = middle + 1;
-            } else if (list.get(middle).compareTo(searchTerm) > 0) {
+            } else if (comp > 0) {
                 high = middle - 1;
-            }
+            } else return middle;
         }
         return -(low + 1);
     }
